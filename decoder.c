e95ad03270d74b693dcdf37730561a49aee1ed59
ponktacology <ponktacology@mailfence.com>
2024-11-09 01:04:28 +0100

    Reading IHDR

--- decoder.c
+++ decoder.c
@@ -7,13 +7,6 @@ FILE* read_file(char *path) {
     return file;
 }
 
-void print_byte_order(int number) {
-    unsigned char *bytes = (unsigned char*)&number;
-    for (int i = 0; i < 4; i++) {
-        printf("%02x ", bytes[i]);    
-    }
-}
-
 void reverse_byte_order(unsigned int *number) {
     unsigned char *bytes = (unsigned char*)number;
     unsigned char byte = bytes[0];
@@ -49,16 +42,16 @@ int decode_header(FILE *file) {
     unsigned char bytes[8];
     int i;
     if ((i = fread(bytes, 1, 8, file)) != 8) {
-        printf("Couldn't load read header %d<8.", i);
+        //printf("Couldn't load read header %d<8.", i);
         return 1;
     }
 
     if ((i = memcmp(bytes, header, 8)) != 0) {
-        printf("Header doesn't match %d.", i);
+        //printf("Header doesn't match %d.", i);
         return 1;
     }
 
-    printf("Decoded header\n");
+    //printf("Decoded header\n");
     return 0;
 }
 
@@ -72,7 +65,7 @@ int decode_chunk_length(FILE *file, Chunk *chunk) {
     }
     reverse_byte_order(&length);
     chunk->length = length; 
-    printf("Decoded length %u\n", length);
+    //printf("Decoded length %u\n", length);
 
     return 0;
 }
@@ -86,7 +79,7 @@ int decode_chunk_type(FILE *file, Chunk *chunk) {
 
     chunk->type[4] = '\0';
 
-    printf("Decoded type %s\n", chunk->type);
+    //printf("Decoded type %s\n", chunk->type);
     return 0;
 }
 
@@ -99,7 +92,7 @@ int decode_chunk_data(FILE *file, Chunk *chunk) {
         return 1;
     }
 
-    printf("Decoded data %c\n", *((char*)chunk->data));
+    //printf("Decoded data %c\n", *((char*)chunk->data));
     return 0;
 }
 
@@ -114,37 +107,71 @@ int decode_chunk_crc(FILE *file, Chunk *chunk) {
     reverse_byte_order(&crc);
     chunk->crc = crc;
 
-    printf("Decoded crc %d\n", crc);
+    //printf("Decoded crc %d\n", crc);
     return 0;
 }
 
-Chunk* decode_chunk(FILE *file) {
+void read_IHDR(Image *image, Chunk *chunk) {
+    char *data = chunk->data;
+
+    unsigned int width = *((unsigned int*) data); data += 4;
+    reverse_byte_order(&width);
+
+    unsigned int height = *((unsigned int*) data); data += 4;
+    reverse_byte_order(&height);
+
+    char depth = *((char*) data); data++;
+    char color = *((char*) data); data++;
+    char compression = *((char*) data); data++;
+    char filter = *((char*) data); data++;
+    char interlace = *((char*) data);
+
+    image->width = width;
+    image->height = height;
+    image->depth = depth;
+    image->color = color;
+    image->compression = compression;
+    image->filter = filter;
+    image->interlace = interlace;
+    
+    printf("width=%u\nheight=%u\ndepth=%d\ncolor=%d\ncompression=%d\nfilter=%d\ninterlace=%d\n",
+           width, height, depth, color, compression, filter, interlace);
+}
+
+Chunk* decode_chunk(Image *image, FILE *file) {
     Chunk *chunk = malloc(sizeof(Chunk));
     if (decode_chunk_length(file, chunk)) return NULL;
     if (decode_chunk_type(file, chunk)) return NULL;
     if (decode_chunk_data(file, chunk)) return NULL;
     if (decode_chunk_crc(file, chunk)) return NULL;
+
+    if (strcmp("IHDR", chunk->type) == 0) {
+        read_IHDR(image, chunk);
+    }
+
     return chunk;
 }
 
-void decode_file(FILE *file) {
+void decode_file(Image *image, FILE *file) {
     if (decode_header(file) != 0) {
         return;
     }
     
     Chunk *chunk;
-    while ((chunk = decode_chunk(file)) != NULL && strcmp(chunk->type, "IEND") != 0);
+    while ((chunk = decode_chunk(image, file)) != NULL && strcmp(chunk->type, "IEND") != 0);
 }
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Usage: %s <file>\n", argv[0]);
+        //printf("Usage: %s <file>\n", argv[0]);
         return 1;
     }
 
     char *fileName = argv[1];
     FILE *file = read_file(fileName);
-    decode_file(file);
+
+    Image image;
+    decode_file(&image, file);
     
     return 0;
 }
